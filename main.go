a17da1904b1b4ff04bb0e396b81faed6c6ed27d1
wangyong <1027480600@qq.com>
2018-08-09 18:38:54 +0800

    未知错误

--- main.go
+++ main.go
@@ -4,19 +4,38 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/zserge/webview"
+	"html/template"
+	"io"
 	"net/http"
 	"strconv"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func startServer() string {
 	port := 8888
 	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("view/*.html")),
+	}
+	e.Renderer = t
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hello world")
+		return c.Render(http.StatusOK, "index", "")
 	})
 
+	e.POST("/search", func(context echo.Context) error {
+		return context.JSON(http.StatusOK, map[string]interface{}{
+			"data": "fuck",
+		})
+	})
 	go e.Start(":" + strconv.Itoa(port))
-	return "http://127.0.0.1:8888"
+	return "http://127.0.0.1:" + strconv.Itoa(port)
 }
 
 func main() {
